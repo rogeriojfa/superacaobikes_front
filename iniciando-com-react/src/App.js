2e19fedd0081953dfe43e26a8008bdd4b9317f69
rogeriojfa <rogeriojfa@gmail.com>
2021-02-22 17:51:04 -0300

    Finalização do Módulo Fundamentos do REact

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./index.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    nome: ""
+  }
+
+  constructor(){
+    super()
+    this.modificarNome = this.modificarNome.bind(this)
+  }
+
+  modificarNome(event){
+      this.setState({
+        nome: event.target.value
+      })
+  }
+
+  criaCombobox = () => {
+    const opcoes = ["Fulano", "Ciclano"]
+
+    const comboboxOpcoes = opcoes.map(opcao => <option>{opcao}</option> )
+
+    return (
+      <select>
+        {comboboxOpcoes}
+      </select>
+    )
+    }
+  
+
+    
+  render(){
+
+    const MeuComboBox = () => this.criaCombobox();
+
+    return (
+      <>
+      <input className="texto-centralizado" type="text" value={this.state.nome} onChange={this.modificarNome}/>
+      <h1>Hello {this.state.nome}</h1>
+      <MeuComboBox />
+      </>
+    ) 
+    }
+  }
+  
 
 export default App;
